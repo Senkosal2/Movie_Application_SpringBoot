4e1923719e4f51015771d3ce080361ad2efd2b77
Senkosal <senkosalker2@gmail.com>
2024-08-23 02:44:15 +0700

    Handle request

--- src/main/java/com/app/movie/controller/MovieController.java
+++ src/main/java/com/app/movie/controller/MovieController.java
@@ -1,18 +1,21 @@
 package com.app.movie.controller;
 
 import com.app.movie.dto.MovieRequest;
-import com.app.movie.exception.NotFoundException;
 import com.app.movie.model.Movie;
 import com.app.movie.service.MovieService;
+import com.app.movie.validator.createGroup;
+import com.app.movie.validator.updateGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/movies")
+@Validated
 public class MovieController {
 
     @Autowired
@@ -33,14 +36,30 @@ public class MovieController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity createNewMovie(@RequestBody MovieRequest movie) {
+    public ResponseEntity createNewMovie(@Validated(createGroup.class) @RequestBody MovieRequest movie) {
         this.movieService.createMovie(movie);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateMovie(@PathVariable("id") Integer id,
+                                      @Validated(updateGroup.class) @RequestBody MovieRequest movie) {
+        movie.setId(id);
+        this.movieService.updateMovie(movie);
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @PostMapping("/create/category")
-    public ResponseEntity createNewMovie(@RequestBody Movie movie) {
-        this.movieService.createMovie(movie);
+    @PatchMapping("/update/{id}")
+    public ResponseEntity updateMoviePartially(@PathVariable("id") Integer id,
+                                               @Validated(updateGroup.class) @RequestBody MovieRequest movie) {
+        movie.setId(id);
+        this.movieService.updateMoviePartially(movie);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteMovie(@PathVariable("id") Integer id) {
+        this.movieService.deleteMovie(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
