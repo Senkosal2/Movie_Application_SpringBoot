2db0854a574c0e100628a963b24bd8738727a9c5
Senkosal <senkosalker2@gmail.com>
2024-08-22 22:09:04 +0700

    Category repository for working with database, one custom method

---
+++ src/main/java/com/app/movie/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.app.movie.repository;
+
+import com.app.movie.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    Category findByCategoryName(String categoryName);
+
+}
