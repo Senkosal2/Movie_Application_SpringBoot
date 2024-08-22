1c90cc8fb165eaa3e19ba9ae59f7f1e5a3bfb297
Senkosal <senkosalker2@gmail.com>
2024-08-23 02:43:26 +0700

    add validation

--- src/main/java/com/app/movie/dto/MovieRequest.java
+++ src/main/java/com/app/movie/dto/MovieRequest.java
@@ -1,13 +1,23 @@
 package com.app.movie.dto;
 
+import com.app.movie.validator.createGroup;
+import com.app.movie.validator.updateGroup;
+import jakarta.validation.constraints.*;
+
 import java.util.List;
-import java.util.Objects;
 
 public class MovieRequest {
 
     private Integer id;
+
+    @NotNull(message = "Title cannot be null!", groups = {createGroup.class})
     private String title;
+
+    @DecimalMin(value = "0.5", message = "Rating must be at least 0.5", groups = {createGroup.class, updateGroup.class})
+    @DecimalMax(value = "5.0", message = "Rating must not exceed 5.0", groups = {createGroup.class, updateGroup.class})
     private Double rating;
+
+    @NotEmpty(message = "Categories cannot be empty", groups = {createGroup.class})
     private List<Object> categories;
 
     public MovieRequest() {}
@@ -27,27 +37,27 @@ public class MovieRequest {
         this.id = id;
     }
 
-    public String getTitle() {
+    public @NotNull(message = "Title cannot be null!") String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(@NotNull(message = "Title cannot be null!") String title) {
         this.title = title;
     }
 
-    public Double getRating() {
+    public @DecimalMin(value = "0.5", message = "Rating must be at least 0.5") @DecimalMax(value = "5.0", message = "Rating must not exceed 5.0") Double getRating() {
         return rating;
     }
 
-    public void setRating(Double rating) {
+    public void setRating(@DecimalMin(value = "0.5", message = "Rating must be at least 0.5") @DecimalMax(value = "5.0", message = "Rating must not exceed 5.0") Double rating) {
         this.rating = rating;
     }
 
-    public List<Object> getCategories() {
+    public @NotEmpty(message = "Categories cannot be empty") List<Object> getCategories() {
         return categories;
     }
 
-    public void setCategories(List<Object> categories) {
+    public void setCategories(@NotEmpty(message = "Categories cannot be empty") List<Object> categories) {
         this.categories = categories;
     }
 }
